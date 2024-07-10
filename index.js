dee630ec6ff293bface35bac328fd88b363f8c04
NgocHaBK <ha.tran1608com@hcmut.edu.vn>
2024-07-10 11:29:49 +0700

    final version 3

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ function GetProductAPI() {
     .then((res) => {
       console.log("get data successfully: ", res.data);
       productsArr = res.data.content;
+      productsArr2 = res.data.content;
       renderProducts();
+      renderProducts2();
     })
     .catch((err) => {
       console.log("error from get all data: ", err);
@@ -11,7 +13,7 @@ function GetProductAPI() {
 }
 GetProductAPI();
 let productsArr = [];
-
+let productsArr2 = [];
 const errorNotify = (param) => {
   const errors = {
     name: "Họ và tên chỉ chứa chữ cái, không chứa chữ số !",
@@ -24,7 +26,33 @@ const errorNotify = (param) => {
   };
   return errors[param];
 };
+const renderProducts2 = (renderedProducts2 = productsArr2) => {
+  try {
+    let contents = "";
+    console.log("data products: ", renderedProducts2);
+    console.log(productsArr2);
 
+    for (let item of renderedProducts2) {
+      const { image, price, name, description } = item;
+      const new_description = description.slice(0, 50);
+      contents += `<div class="flex-column mt-4 sale_item">
+            <a href="./detail.html?productid=${item.id}"><img src="${image}" alt="hinh" class="w-100 img_product"/></a>
+            <a href="./detail.html?productid=${item.id}"><p class="shoes_name">${name}</p></a>
+            <p class="shoes_price" >$ ${price}</p>
+          </div>`;
+    }
+    console.log("Generated HTML content: ", contents);
+
+    const detailedProductsElement = document.getElementById("sale_products");
+    if (!detailedProductsElement) {
+      throw new Error("Element with ID 'sale_products' not found");
+    }
+    detailedProductsElement.innerHTML = contents;
+    console.log("Products rendered successfully");
+  } catch (error) {
+    console.error("Error in renderProducts: ", error.message);
+  }
+};
 const renderProducts = (renderedProducts = productsArr) => {
   try {
     let contents = "";
@@ -34,8 +62,8 @@ const renderProducts = (renderedProducts = productsArr) => {
       const { image, price, name, description } = item;
       const new_description = description.slice(0, 50);
       contents += `<div class="flex-column mt-4 item col">
-            <a href="./detail.html?productid=${item.id}"><img src="${image}" alt="hinh" style="background-color: #f5f5f5;object-fit:contain" class="w-100"/></a>
-            <a href="./detail.html?productid=${item.id}" style="color:#f15e2c"><p>${name}</p></a>
+            <a href="./detail.html?productid=${item.id}"><img src="${image}" alt="hinh" style="background-color: #f5f5f5;object-fit:contain" class=" img_product"/></a>
+            <a href="./detail.html?productid=${item.id}" style="color:#f15e2c" class="mt-2 fs-5 d-inline-block"><p>${name}</p></a>
             <p class="shoes_name pb-0 mb-2" style="color:white">Men's shoes</p>
             <p class="shoes_description">${new_description}</p>
             <p class="pt-0">$ ${price}</p>
